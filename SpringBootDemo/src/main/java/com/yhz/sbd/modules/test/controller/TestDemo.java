2747ec82d31964b6b7b7a105450ad5219cf0dd63
Administrator <Administrator@IMEX8H3ER0Q16W1>
2020-04-30 10:45:31 +0800

    "File upload and downLoad"

--- SpringBootDemo/src/main/java/com/yhz/sbd/modules/test/controller/TestDemo.java
+++ SpringBootDemo/src/main/java/com/yhz/sbd/modules/test/controller/TestDemo.java
@@ -1,12 +1,25 @@
 package com.yhz.sbd.modules.test.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -68,12 +81,101 @@ public class TestDemo {
 
 	/**
 	 * 
-	 * 搭建的springboot框架
-	 * http://127.0.0.1/test/Hello?key=fuck
+	 * 搭建的springboot框架 http://127.0.0.1/test/Hello?key=fuck
 	 */
 	@RequestMapping("/Hello")
-	public String getName(HttpServletRequest request ,@RequestParam String key) {
+	public String getName(HttpServletRequest request, @RequestParam String key) {
 		String value2 = request.getParameter("value");
 		return "Hello world,this is spring boot demo" + key + "===" + value2;
+	}
+
+	/**
+	 * 文件的下载
+	 */
+
+	@RequestMapping("/download")
+	public ResponseEntity<Resource> downLoadFile(@RequestParam String fileName) {
+		try {
+			Resource resource = new UrlResource(Paths.get("D:\\fileupload\\" + fileName).toUri());
+
+			return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
+					.header(HttpHeaders.CONTENT_DISPOSITION,
+							String.format("attachment; filename=\"%s\"", resource.getFilename()))
+					.body(resource);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * 将文件以BufferedInputStream的方式读取到byte[]里面，然后用OutputStream.write输出文件
+	 */
+	@RequestMapping("/download1")
+	public void downloadFile1(HttpServletRequest request, HttpServletResponse response, @RequestParam String fileName) {
+		String filePath = "D:/fileupload" + File.separator + fileName;
+		File downloadFile = new File(filePath);
+
+		if (downloadFile.exists()) {
+			response.setContentType("application/octet-stream");
+			response.setContentLength((int) downloadFile.length());
+			response.setHeader(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=\"%s\"", fileName));
+
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = null;
+			BufferedInputStream bis = null;
+			try {
+				fis = new FileInputStream(downloadFile);
+				bis = new BufferedInputStream(fis);
+				OutputStream os = response.getOutputStream();
+				int i = bis.read(buffer);
+				while (i != -1) {
+					os.write(buffer, 0, i);
+					i = bis.read(buffer);
+				}
+			} catch (Exception e) {
+				LOGGER.debug(e.getMessage());
+				e.printStackTrace();
+			} finally {
+				try {
+					if (fis != null) {
+						fis.close();
+					}
+					if (bis != null) {
+						bis.close();
+					}
+				} catch (Exception e2) {
+					LOGGER.debug(e2.getMessage());
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * 以包装类 IOUtils 输出文件
+	 */
+	@RequestMapping("/download2")
+	public void downloadFile2(HttpServletRequest request, HttpServletResponse response, @RequestParam String fileName) {
+		String filePath = "D:/fileupload" + File.separator + fileName;
+		File downloadFile = new File(filePath);
+
+		try {
+			if (downloadFile.exists()) {
+				response.setContentType("application/octet-stream");
+				response.setContentLength((int) downloadFile.length());
+				response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+						String.format("attachment; filename=\"%s\"", fileName));
+
+				InputStream is = new FileInputStream(downloadFile);
+				IOUtils.copy(is, response.getOutputStream());
+				response.flushBuffer();
+			}
+		} catch (Exception e) {
+			LOGGER.debug(e.getMessage());
+			e.printStackTrace();
+		}
+
 	}
 }
--- SpringBootDemo/src/main/java/com/yhz/sbd/modules/test/controller/testPageController.java
+++ SpringBootDemo/src/main/java/com/yhz/sbd/modules/test/controller/testPageController.java
@@ -1,12 +1,18 @@
 package com.yhz.sbd.modules.test.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.yhz.sbd.modules.test.entity.City;
 import com.yhz.sbd.modules.test.entity.Country;
@@ -51,5 +57,73 @@ public class testPageController {
 		
 		
 	}
+	
+	/**
+	 * 单个文件的上传
+	 */
+	@PostMapping(value="/upload", consumes = "multipart/form-data")
+	public String uploadFile(@RequestParam MultipartFile file, 
+			RedirectAttributes redirectAttributes){
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "Please select file.");
+			return "redirect:/test/index";
+		}
 
+		try {
+			String fileName = file.getOriginalFilename();
+			String destFilePath = "D:\\fileupload\\" + fileName;
+			File destFile = new File(destFilePath);
+			file.transferTo(destFile);
+
+			// 使用工具类Files来上传文件
+//			byte[] bytes = file.getBytes();
+//			Path path = Paths.get(destFileName);
+//			Files.write(path, bytes);
+
+			redirectAttributes.addFlashAttribute("message", "Upload file success.");
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload file failed.");
+			return "redirect:/test/index";
+		}
+
+		return "redirect:/test/index";
+		
+	}
+	
+	/**
+	 * 上传多个文件
+	 */
+	@PostMapping(value="/uploadfiles", consumes="multipart/form-data")
+	public String uploadFiles(@RequestParam MultipartFile[] files, 
+			RedirectAttributes redirectAttributes) {
+		boolean isEmpty = true;
+
+		try {
+			for (MultipartFile file : files) {
+				if (file.isEmpty()) {
+//				break;
+					continue;
+				}
+
+				String fileName = file.getOriginalFilename();
+				String destFilePath = "D:\\fileupload\\" + fileName;
+				File destFile = new File(destFilePath);
+				file.transferTo(destFile);
+
+				isEmpty = false;
+			}
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload file failed.");
+			return "redirect:/test/index";
+		}
+
+		if (isEmpty) {
+			redirectAttributes.addFlashAttribute("message", "Please select file.");
+		} else {
+			redirectAttributes.addFlashAttribute("message", "Upload file success.");
+		}
+		return "redirect:/test/index";
+	}
 }
